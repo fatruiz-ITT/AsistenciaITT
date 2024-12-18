57180a3816e065c658f8a92ea8a07f49ac3778cf
fatima ruiz <ruizgarciafatimadejesus@gmail.com>
2024-12-17 18:33:22 -0800

    Update codigo.js
    update tabla modificacion

--- codigo.js
+++ codigo.js
@@ -658,7 +658,7 @@ async function obtenerDatosGoogleSheets() {
             // Mapear las filas del JSON a un formato más usable
             datosOriginales = jsonData.table.rows.map(row => ({
                 Empresa: row.c[4]?.v || '',  // Columna A
-                Grupo: row.c[3]?.v || '',    // Columna B
+                Grupo: row.c[6]?.v || '',    // Columna B
                 B: row.c[1]?.v || '',        // Columna B (repitiendo por simplicidad)
                 C: row.c[2]?.v || '',        // Columna C
                 D: row.c[3]?.v || '',        // Columna D
